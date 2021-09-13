e2b72e0d26a777aebe7a6bbe6ce60f0c8113096d
Juan Porta <jporta@c1r1s2.42madrid.com>
2021-09-13 16:04:13 +0200

    primera

---
+++ libft.a/src/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jporta <jporta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 15:34:47 by jporta            #+#    #+#             */
+/*   Updated: 2021/09/13 15:58:05 by jporta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdio.h>
+
+int		ft_isalnum(int c);
+size_t	ft_strlen(const char *str);
+
+#endif
